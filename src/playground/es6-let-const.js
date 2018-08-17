aeec6ff70225a51b80b64c4c2589410b381b3401
lakshmi.rm <lakshmi.rm@inapp.com>
2018-08-17 14:43:32 +0530

    let const es6 functionality.

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -4,20 +4,19 @@ import React, { Component } from 'react';
 export class SampleApp extends Component {
 
     render() {
-        //var namevar = "rml";
-        // var namevar = "***"; //var more initalization
-        //let namelet = "rm";   //must one time intializ 
-        //namelet = "valid let";
-        //const nameconst = "free";
-        //console.log(nameconst, "nameconst");
+        var namevar = "rml";
+        var namevar = "***"; //using var - more initalization
+        let namelet = "rm";   //using let - must one time intialization
+        namelet = "valid let";
+        const nameconst = "free";
+        console.log(nameconst, "nameconst");
 
-        // function getpetname() {
-        //     const petname = "kukku"
-        //     // console.log(petname,"-----")
-        //     return petname;
-        // }
-        // const petname = getpetname();
-        // console.log(petname,"*************")
+        function getpetname() {
+            const petname = "kukku";
+            return petname;
+        }
+        const petname = getpetname();   //using const - must 1 init and 1 declaration
+        console.log(petname,"****")
 
         //block scoping
         const fullname = "lakshmi rm";
